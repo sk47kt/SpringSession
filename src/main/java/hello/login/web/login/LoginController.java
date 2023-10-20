ae51b489c0ec8e4f800ac765b0509cb95cae2482
sk47k <sk47kt@naver.cokm>
2023-10-20 17:59:27 +0900

    Initial commit

---
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -0,0 +1,17 @@
+package hello.login.web.login;
+
+import hello.login.domain.login.LoginService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @GetMapping
+}
